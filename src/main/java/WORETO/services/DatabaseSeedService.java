8c3196f23fa0d82252dee57454f8a35128e89b29
Nerok96 <nerok96@gmail.com>
2020-07-12 10:39:02 +0200

    New methods for Seeder, need to create new no Reactive Repositories in order to create the seeder

--- src/main/java/WORETO/services/DatabaseSeedService.java
+++ src/main/java/WORETO/services/DatabaseSeedService.java
@@ -1,7 +1,10 @@
 package WORETO.services;
 
+import WORETO.repositories.ProjectReactrepository;
 import WORETO.repositories.TimeRegistryReactRepository;
+import WORETO.repositories.UserReactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -9,11 +12,25 @@ import javax.annotation.PostConstruct;
 @Service
 public class DatabaseSeedService {
 
+    @Value("${miw.admin.email}")
+    private String email;
+    @Value("${ miw.admin.name}")
+    private String name;
+    @Value("${miw.admin.password}")
+    private String password;
+
+    //TODO change react repositories for synchronous ones
     private TimeRegistryReactRepository timeRegistryReactRepository;
+    private ProjectReactrepository projectReactrepository;
+    private UserReactRepository userReactRepository;
 
     @Autowired
-    public DatabaseSeedService(TimeRegistryReactRepository timeRegistryReactRepository){
+    public DatabaseSeedService(TimeRegistryReactRepository timeRegistryReactRepository,
+                               ProjectReactrepository projectReactrepository,
+                               UserReactRepository userReactRepository){
         this.timeRegistryReactRepository = timeRegistryReactRepository;
+        this.projectReactrepository = projectReactrepository;
+        this.userReactRepository = userReactRepository;
     }
 
     @PostConstruct
@@ -29,12 +46,14 @@ public class DatabaseSeedService {
     public void deleteAllAndInitialize() {
         // Delete Repositories -----------------------------------------------------
         this.timeRegistryReactRepository.deleteAll();
+        this.projectReactrepository.deleteAll();
+        this.userReactRepository.deleteAll();
         // -------------------------------------------------------------------------
         this.initialize();
     }
 
     private void initialize() {
-        // TODO
+
     }
 
     public void seedDataBaseJava() {
