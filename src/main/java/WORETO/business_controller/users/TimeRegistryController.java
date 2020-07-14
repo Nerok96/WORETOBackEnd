0c8586cf5d8b57f96f0bb59c1ee229df75117ad7
Nerok96 <nerok96@gmail.com>
2020-07-14 16:05:27 +0200

    New controller method for Create User UseCase. Also removed unused imports from old Dtos.

--- src/main/java/WORETO/business_controller/users/TimeRegistryController.java
+++ src/main/java/WORETO/business_controller/users/TimeRegistryController.java
@@ -1,7 +1,15 @@
 package WORETO.business_controller.users;
 
+import WORETO.documents.Project;
+import WORETO.documents.TimeRegistry;
+import WORETO.documents.User;
+import WORETO.dtos.TimeRegistryCreationDto;
 import WORETO.dtos.TimeRegistryReadDetailDto;
+import WORETO.repositories.ProjectReactrepository;
 import WORETO.repositories.TimeRegistryReactRepository;
+import WORETO.repositories.UserReactRepository;
+import WORETO.repositories.UserRepository;
+import WORETO.services.SequenceGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -10,14 +18,40 @@ import reactor.core.publisher.Mono;
 public class TimeRegistryController {
 
     private TimeRegistryReactRepository timeRegistryReactRepository;
+    private UserReactRepository userReactRepository;
+    private ProjectReactrepository projectReactrepository;
+    private SequenceGenerator sequenceGenerator;
 
     @Autowired
-    public TimeRegistryController(TimeRegistryReactRepository timeRegistryReactRepository){
+    public TimeRegistryController(TimeRegistryReactRepository timeRegistryReactRepository,
+                                  UserReactRepository userReactRepository,
+                                  ProjectReactrepository projectReactrepository,
+                                  SequenceGenerator sequenceGenerator) {
         this.timeRegistryReactRepository = timeRegistryReactRepository;
+        this.userReactRepository = userReactRepository;
+        this.projectReactrepository = projectReactrepository;
+        this.sequenceGenerator = sequenceGenerator;
     }
 
     public Mono<TimeRegistryReadDetailDto> readTimeRegistryDetailById(String id) {
         return this.timeRegistryReactRepository.findById(id).map(TimeRegistryReadDetailDto::new);
     }
 
+    public Mono<TimeRegistryReadDetailDto> createTimeRegistry(TimeRegistryCreationDto timeRegistryCreationDto) {
+        TimeRegistry timeRegistry = TimeRegistry.builder()
+                .id(sequenceGenerator.getNextSequence(TimeRegistry.SEQUENCE_NAME))
+                .assignedLocalDateTime(timeRegistryCreationDto.getAssignedLocalDateTime())
+                .minutesWorked(timeRegistryCreationDto.getMinutesWorked())
+                .status(timeRegistryCreationDto.getStatus())
+                .description(timeRegistryCreationDto.getDescription())
+                .build();
+        Mono<User> assignedUserMono = this.userReactRepository.findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
+                .doOnNext(timeRegistry::setAssignedUser);
+        Mono<User> createdByUserMono = this.userReactRepository.findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
+                .doOnNext(timeRegistry::setCreatedByUser);
+        Mono<Project> projectMono = this.projectReactrepository.findById(timeRegistryCreationDto.getAssignedProjectId())
+                .doOnNext(timeRegistry::setAssignedProject);
+        return Mono.when(assignedUserMono, createdByUserMono, projectMono)
+                .then(this.timeRegistryReactRepository.save(timeRegistry).map(TimeRegistryReadDetailDto::new));
+    }
 }
