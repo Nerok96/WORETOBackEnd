13044eb3e34458612ccea16c7e5aff43206d68cf
Nerok96 <nerok96@gmail.com>
2020-07-14 11:47:17 +0200

    HotFix remove unused imports and repository

--- src/main/java/WORETO/business_controller/systems/ValidateUserController.java
+++ src/main/java/WORETO/business_controller/systems/ValidateUserController.java
@@ -2,22 +2,12 @@ package WORETO.business_controller.systems;
 
 import WORETO.documents.User;
 import WORETO.dtos.UserDto;
-import WORETO.repositories.UserReactRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
 @Controller
 public class ValidateUserController {
-
-    private UserReactRepository userReactRepository;
-
-    @Autowired
-    public ValidateUserController(UserReactRepository userReactRepository) {
-        this.userReactRepository = userReactRepository;
-    }
 
     public Boolean validateUser(User user) {
         return validateUser(new UserDto(user));
