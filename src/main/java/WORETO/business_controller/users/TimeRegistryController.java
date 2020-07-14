9cea46139e84b0bf2a77fe82abc29426a910e06c
Nerok96 <nerok96@gmail.com>
2020-07-14 17:01:20 +0200

    New controller for Update Time Registry UseCase

--- src/main/java/WORETO/business_controller/users/TimeRegistryController.java
+++ src/main/java/WORETO/business_controller/users/TimeRegistryController.java
@@ -5,6 +5,7 @@ import WORETO.documents.TimeRegistry;
 import WORETO.documents.User;
 import WORETO.dtos.TimeRegistryCreationDto;
 import WORETO.dtos.TimeRegistryReadDetailDto;
+import WORETO.dtos.TimeRegistryUpdateDto;
 import WORETO.repositories.ProjectReactrepository;
 import WORETO.repositories.TimeRegistryReactRepository;
 import WORETO.repositories.UserReactRepository;
@@ -12,6 +13,8 @@ import WORETO.services.SequenceGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class TimeRegistryController {
@@ -48,14 +51,53 @@ public class TimeRegistryController {
                     .status(timeRegistryCreationDto.getStatus())
                     .description(timeRegistryCreationDto.getDescription())
                     .build();
-            Mono<User> assignedUserMono = this.userReactRepository.findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
+            Mono<User> assignedUserMono = this.userReactRepository
+                    .findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
                     .doOnNext(timeRegistry::setAssignedUser);
-            Mono<User> createdByUserMono = this.userReactRepository.findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
+            Mono<User> createdByUserMono = this.userReactRepository
+                    .findByEmail(timeRegistryCreationDto.getAssignedUserEmail())
                     .doOnNext(timeRegistry::setCreatedByUser);
-            Mono<Project> projectMono = this.projectReactrepository.findById(timeRegistryCreationDto.getAssignedProjectId())
+            Mono<Project> projectMono = this.projectReactrepository
+                    .findById(timeRegistryCreationDto.getAssignedProjectId())
                     .doOnNext(timeRegistry::setAssignedProject);
             return Mono.when(assignedUserMono, createdByUserMono, projectMono)
                     .then(this.timeRegistryReactRepository.save(timeRegistry).map(TimeRegistryReadDetailDto::new));
+        } else {
+            return null;
+        }
+    }
+
+    public Mono<TimeRegistryReadDetailDto> updateTimeRegistry(TimeRegistryUpdateDto timeRegistryUpdateDto) {
+        if (validateTimeRegistryController.validateDraftStatus(timeRegistryUpdateDto)) {
+            TimeRegistry timeRegistryFromUpdateDto = TimeRegistry.builder()
+                    .assignedLocalDateTime(timeRegistryUpdateDto.getAssignedLocalDateTime())
+                    .minutesWorked(timeRegistryUpdateDto.getMinutesWorked())
+                    .status(timeRegistryUpdateDto.getStatus())
+                    .description(timeRegistryUpdateDto.getDescription())
+                    .lastModifiedLocalDateTime(LocalDateTime.now())
+                    .build();
+            Mono<User> assignedUserMono = this.userReactRepository
+                    .findByEmail(timeRegistryUpdateDto.getAssignedUserEmail())
+                    .doOnNext(timeRegistryFromUpdateDto::setAssignedUser);
+            Mono<User> lastModifiedByUserMono = this.userReactRepository
+                    .findByEmail(timeRegistryUpdateDto.getLastModifiedByUserEmail())
+                    .doOnNext(timeRegistryFromUpdateDto::setAssignedUser);
+            Mono<Project> assignedProjectMono = this.projectReactrepository
+                    .findById(timeRegistryUpdateDto.getAssignedProjectId())
+                    .doOnNext(timeRegistryFromUpdateDto::setAssignedProject);
+            Mono<TimeRegistry> timeRegistryMono =
+                    Mono.when(assignedUserMono, assignedProjectMono, lastModifiedByUserMono).
+                            then(this.timeRegistryReactRepository
+                                    .findById(timeRegistryUpdateDto.getId())
+                                    .map(timeRegistry -> {
+                                        timeRegistry = timeRegistryFromUpdateDto;
+                                        return timeRegistry;
+                                    })
+                            );
+            return Mono.when(timeRegistryMono)
+                    .then(this.timeRegistryReactRepository.saveAll(timeRegistryMono)
+                            .next())
+                    .map(TimeRegistryReadDetailDto::new);
         } else {
             return null;
         }
